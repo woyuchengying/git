bfa7d276ac8a6a68af43cb8584ca571f55d12671
我欲成鹰 <woyuchengying@aliyun.com>
2017-04-18 11:31:55 +0800

    Update base.sh

--- ansible/base.sh
+++ ansible/base.sh
@@ -8,5 +8,7 @@ ansible center-slave -m shell -a "source /root/.bashrc"
 ansible center-slave -m get_url -a "url=https://nmap.org/dist/ncat-7.40-1.x86_64.rpm use_proxy=yes dest=/root/package_dir/"
 3.卸载nc
 ansible center-slave -m yum -a "name=nc  state=absent"
-4.安装ncat，查看状态
+4.安装ncat，查看状态, 测试端口连通性
 ansible center-slave -m yum -a "name=/root/package_dir/ncat-7.40-1.x86_64.rpm state=present"
+ansible center-slave -m shell -a "ncat --version"
+ansible center-slave -m shell -a "ncat -z -v 20.26.25.187 80"
