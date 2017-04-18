c1c84fbd9fcc2028e707669ca098994cf6b438fc
我欲成鹰 <woyuchengying@aliyun.com>
2017-04-18 11:30:05 +0800

    Create base.sh

---
+++ ansible/base.sh
@@ -0,0 +1,12 @@
+1.配置代理
+ansible center-slave -m shell -a "echo 'export http_proxy=http://proxy.zj.chinamobile.com:8080
+export https_proxy=http://proxy.zj.chinamobile.com:8080
+export ftp_proxy=http://proxy.zj.chinamobile.com:8080' >> /root/.bashrc"
+
+ansible center-slave -m shell -a "source /root/.bashrc"
+2.下载ncat
+ansible center-slave -m get_url -a "url=https://nmap.org/dist/ncat-7.40-1.x86_64.rpm use_proxy=yes dest=/root/package_dir/"
+3.卸载nc
+ansible center-slave -m yum -a "name=nc  state=absent"
+4.安装ncat，查看状态
+ansible center-slave -m yum -a "name=/root/package_dir/ncat-7.40-1.x86_64.rpm state=present"
